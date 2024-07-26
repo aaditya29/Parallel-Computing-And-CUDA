8f7485c428fe514407a605687e37d4be23747326
Aditya Mishra <mishraaaditya29@gmail.com>
2024-07-26 16:13:16 +0530

    git commit

--- 1-Paralleism-In-CPP/1-Introduction/Introduction.cpp
+++ 1-Paralleism-In-CPP/1-Introduction/Introduction.cpp
@@ -1,25 +1,27 @@
+
 #include <iostream>
 #include<thread> //library for the thread
-using namespace std;
+#include<vector>
+
 
 //making initial function for the each thread
 void call_from(int tid){
-    cout<<"Launched by the thread: "<<tid<<endl;
+    std::cout<<"Launched by the thread: "<<tid<<std::endl;
 }
 
 int main(){
     //Creating an array of 10 thread objects
-    thread t[10];
+    std::thread t[10];
 
     //Launching 10 threads from the initial function "call_from"
     //threads need a function to initialise them
     for(int i = 0; i < 10; i++){
-        t[i] = thread(call_from, i);//Calling constructor for thread with passing the thread i
+        t[i] = std::thread(call_from, i);//Calling constructor for thread with passing the thread i
 
     }
 
     //Printing from the main function
-    cout<<"Launched the threads from the main() "<<endl;
+    std::cout<<"Launched the threads from the main() "<<std::endl;
 
     //Waiting for all the threads to finish
     /*

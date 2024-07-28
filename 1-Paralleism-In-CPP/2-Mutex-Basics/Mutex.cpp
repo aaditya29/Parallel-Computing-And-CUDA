52e2b4a92431d81e43e3742203f4b0a9f4f7e9bb
Aditya Mishra <mishraaaditya29@gmail.com>
2024-07-28 18:49:59 +0530

    added mutex lock function

--- 1-Paralleism-In-CPP/2-Mutex-Basics/Mutex.cpp
+++ 1-Paralleism-In-CPP/2-Mutex-Basics/Mutex.cpp
@@ -6,3 +6,10 @@ using namespace std;
 
 //Adding global mutex lock for the critical section
 mutex mtx;
+
+//Initial function for each thread
+void call_from(int tid){
+    mtx.lock();
+    cout<<"Launched by thread: "<<tid<<endl;
+    mtx.unlock();
+}

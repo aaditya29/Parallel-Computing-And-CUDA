e38ed02947b2a26e25f0e6c15dc94086fb14381f
Aditya Mishra <mishraaaditya29@gmail.com>
2024-07-28 18:54:24 +0530

    launched 10 threads

--- 1-Paralleism-In-CPP/2-Mutex-Basics/Mutex.cpp
+++ 1-Paralleism-In-CPP/2-Mutex-Basics/Mutex.cpp
@@ -13,3 +13,15 @@ void call_from(int tid){
     cout<<"Launched by thread: "<<tid<<endl;
     mtx.unlock();
 }
+
+int main(){
+    //Creating an array of 10 thread objects
+    thread t[10];
+
+    //Launching 10 threads from the initial function call_from
+    for(int i = 0; i<10; i++){
+        t[i] = thread(call_from, i);
+    }
+
+    
+}

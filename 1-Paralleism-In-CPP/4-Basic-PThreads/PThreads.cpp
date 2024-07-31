4a680c9c2a9213b7517aff36bfb4c59f50eabfb9
Aditya Mishra <mishraaaditya29@gmail.com>
2024-07-31 14:39:19 +0530

    added thread function

--- 1-Paralleism-In-CPP/4-Basic-PThreads/PThreads.cpp
+++ 1-Paralleism-In-CPP/4-Basic-PThreads/PThreads.cpp
@@ -6,3 +6,28 @@
 using namespace std;
 
 #define NUM_THREADS 10
+
+//Global pthread implementation of mutex lock
+pthread_mutex_t mtx;
+
+//Here we are creating Args named struct
+// Args will be casted as void* for or thread function
+struct Args {
+    int tid;
+    int data;
+
+};
+
+// pthread function must have return type void*, with a void* arg
+void *boring_thread_function(void *args){
+    Args *local_args = (Args*)args;
+    //Getting the lock before entering the critical section
+    pthread_mutex_lock(&mtx);
+    //Entering critical section
+    cout<<"Printing from the thread "<<local_args->tid<<endl;
+    cout<<"Value of the data is "<<local_args->data<<endl;
+    pthread_mutex_unlock(&mtx);
+}
+
+
+
